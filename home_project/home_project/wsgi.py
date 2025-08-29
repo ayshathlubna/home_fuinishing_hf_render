5572743caed121acd2be394314844a84c2074451
ayshathlubna <146834799+ayshathlubna@users.noreply.github.com>
2025-08-30 00:41:57 +0530

    Update wsgi.py

--- home_project/home_project/wsgi.py
+++ home_project/home_project/wsgi.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_project.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_project.home_project.settings')
 application = get_wsgi_application()

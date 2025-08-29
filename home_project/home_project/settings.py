cdb1272cf5eca4d6ec648642f5373e6848f54e2c
ayshathlubna <146834799+ayshathlubna@users.noreply.github.com>
2025-08-29 15:27:48 +0530

    Update settings.py

--- home_project/home_project/settings.py
+++ home_project/home_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wltnklmyjj=%#1z_yx2l1-yx-d$0ghz&i10p03v%#2%z%2@*n9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
